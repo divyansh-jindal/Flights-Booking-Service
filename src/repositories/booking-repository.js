fd098ee7935b4fd626803805c692b337c40a24d3
Divyansh Jindal <divyanshjindal18@gmail.com>
2023-11-22 15:34:50 +0530

    Started booking impl

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 const { Booking } = require('../models');
-const { CrudRepository } = require('./crud-repository');
+const CrudRepository = require('./crud-repository');
 
 class BookingRepository extends CrudRepository{
     constructor(){
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,42 @@
-module.exports={
+const axios = require('axios');
+
+const { ServerConfig } = require('../config');
+const { BookingRepository } = require('../repositories');
+const db = require('../models');
+const AppError = require('../utils/errors/app-error');
+const { StatusCodes } = require('http-status-codes');
+
+async function createBooking(data) {
+    return new Promise((resolve,reject)=>{
+        const result = db.sequelize.transaction(async function bookingImpl(t){
+            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            console.log(typeof flight);
+            const FlightData = flight.data.data;
+            if(data.noOfSeats>FlightData.totalSeats){
+                reject (new AppError('Not enough seats available', StatusCodes.BAD_REQUEST));
+            }
+            // console.log(flight.data);
+            resolve (true);
+        });
+    })    
+    // try {
+    //     const result = db.sequelize.transaction(async function bookingImpl(t){
+    //         const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+    //         const FlightData = flight.data.data;
+    //         if(data.noOfSeats>FlightData.totalSeats){
+    //             throw new AppError('Not enough seats available', StatusCodes.BAD_REQUEST);
+    //         }
+    //         console.log(flight.data);
+    //         return true;
+    //     });
+    // } catch(error) {
+    //     console.log(error);
+    //     // throw new AppError(error.message, StatusCodes.INTERNAL_SERVER_ERROR);
+    //     throw error;
+    // }
     
+}
+
+module.exports={
+    createBooking
 }
